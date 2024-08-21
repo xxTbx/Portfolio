a2127d0b92b9632577a7c630c025bcc9ea206287
Tb <toqir.bhatti@gmail.com>
2024-08-21 17:53:40 +0200

    update set1

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
           <p className="section-title">About</p>
           <h1 className="section-heading">About Me</h1>
           <p className="main-section-description">
-          I am currently pursuing a Master's in Applied Computer Science, building on my Bachelor's degree in Information Systems. My passion for PCs and IT has been a driving force since a young age, which has led me to develop strong skills in these areas. With a solid foundation in developing, security and related fields, I am continuously expanding my knowledge and abilities, aiming to solve complex challenges in the tech industry 
+          I am currently pursuing a Master's in Applied Computer Science, building on my Bachelor's degree in Information Systems. My passion for PCs and IT has been a driving force since a young age, which has led me to develop strong skills in these areas. With a solid foundation in developing, security and related fields, I am continuously expanding my knowledge and abilities. 
           </p>
         </div>
       </div>
